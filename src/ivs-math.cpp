5604acbfd13bfa06b93656d14f91020434a0573d
Zdeněk Borovec <zdenda.borovec@gmail.com>
2023-04-08 16:10:36 +0200

    added exponentiate implementation

--- src/ivs-math.cpp
+++ src/ivs-math.cpp
@@ -21,7 +21,23 @@ double divide(double p_dividend, double p_divisor)
 {
 	if(p_divisor == 0)
 	{
-		throw std::invalid_argument("Cannot divide by 0 MORON!!!!");
+		throw std::invalid_argument("cannot divide by 0 moron!!!!");
 	}
 	return p_dividend / p_divisor;
+}
+
+double exponentiate(double p_base, double p_exponent)
+{
+	if(p_exponent < 0)
+	{
+		throw std::invalid_argument("The exponent has to be a natural number.");
+	}
+	else if(p_exponent == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return p_base * exponentiate(p_base, p_exponent - 1);
+	}
 }
